f69c8f7dd024bbea9eaa3bb07fd24004a70d609b
ezmiller-su <ezrajmiller@hotmail.com>
2026-04-15 17:11:20 -0400

    sorted csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,10 @@ def format_currency(amount):
 
 st.set_page_config(page_title='Derby Days 2026')
 
-livestats = pd.read_csv('https://raw.githubusercontent.com/ezmiller-su/derbydays2026/refs/heads/main/derbydays_donations_2025.csv')
+stats = pd.read_csv('https://raw.githubusercontent.com/ezmiller-su/derbydays2026/refs/heads/main/derbydays_donations_2026.csv')
 if st.sidebar.button(label='Refresh Data'):
-    livestats = pd.read_csv('https://raw.githubusercontent.com/ezmiller-su/derbydays2026/refs/heads/main/derbydays_donations_2025.csv')
-oldstats = pd.read_csv('scraper/derbydays_donations_2025.csv')
+    stats = pd.read_csv('https://raw.githubusercontent.com/ezmiller-su/derbydays2026/refs/heads/main/derbydays_donations_2026.csv')
 
-stats = pd.concat([livestats, oldstats])
 stats['Timestamp'] = pd.to_datetime(stats['Timestamp'], format='%Y-%m-%d %H:%M:%S')
 
 now = datetime.now()
